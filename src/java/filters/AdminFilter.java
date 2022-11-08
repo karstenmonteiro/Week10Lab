627faef96498c833ef1ccd9bf91434b6811f2dc6
karstenmonteiro <karstenmonteiro@kmLaptop>
2022-11-07 23:06:39 -0700

    Week 10 Lab: Security (initial commit) The initial commit for the Week 10 Lab: Security.

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,41 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Karsten Monteiro
+ */
+public class AdminFilter implements Filter {
+   
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+            
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+            
+            // execute the servlet
+            chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+}
